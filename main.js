6b96d9518c7be50dfed3f97b755a29fc0f92e321
Valera Horsharik <valera.horsharik@gmail.com>
2016-09-12 22:19:47 +0300

    Create ajax function which select options for <select #id=country> on page load

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-$('#hello').submit(function () {
-    var FormData = {
-        "name":$('#name').val(),
-        "surname":$('#surname').val()
-    };
-    
+$(document).ready(function(){
     $.ajax({
         type: "POST",
-        url: 'hello.php',
-        data: {
-        name:$('#name').val(),
-        surname:$('#surname').val()
-    },
+        url: 'data.php',
+        data:{"select":"country"},
         success: function (data) {
             data = JSON.parse(data);
             for(var i in data){
-                $('#results').append(data[i]);
+                $('#country').append("<option value=\"1\">"+data[i]+"</option>\n");
             }
         }
     });
-    console.log(FormData);
-    return false;
+   
 });
+//$('#hello').on('submit',function () {
+//    var FormData = {
+//        "name":$('#name').val(),
+//        "surname":$('#surname').val()
+//    };
+//    
+//    $.ajax({
+//        type: "POST",
+//        url: 'hello.php',
+//        data: {
+//        name:$('#name').val(),
+//        surname:$('#surname').val()
+//    },
+//        success: function (data) {
+//            data = JSON.parse(data);
+//            for(var i in data){
+//                $('#results').append(data[i]);
+//            }
+//        }
+//    });
+//    console.log(FormData);
+//    return false;
+//});
