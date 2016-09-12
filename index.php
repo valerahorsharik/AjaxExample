2f8bd1ffc4a6c55def9aaa42da8a58c79fbd241a
Valera Horsharik <valera.horsharik@gmail.com>
2016-09-12 22:17:47 +0300

    Change <select> for test

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
+<?php
+$country=["Ru","Ua","Us","Fr"];
+echo json_encode($country);
+?>
 <html>
     <head>
         <meta charset="utf-8">  
     </head>
     <body>
-        <form id="hello" >
-            <input type="text" id="name" name="name"><br/><br/>
-            <input type="text" id="surname" name="surname"><br/><br/>
-            <button type="submit" >Отправить</button>
-        </form>
-        <div id="results"></div>
+        <div class="options">Call centr: 
+            <select id="country">
+            </select>
+        </div>
+        <div class="options">Call centr: 
+            <select id="city">
+            </select>
+        </div>
+        <div class="options">Call centr: 
+            <select>
+                <option value="1">KievExpert</option>
+                <option value="1">ZaporozhyeExpert</option>
+            </select>
+        </div>
+        <div class="options">Call centr: 
+            <select>
+                <option value="1">KievExpert</option>
+                <option value="1">ZaporozhyeExpert</option>
+            </select>
+        </div>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script type="text/javascript" src="main.js"></script>
     </body>
