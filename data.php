b6dc34610f9bcf7de503499a2a2ef85663262bb0
Valera Horsharik <valera.horsharik@gmail.com>
2016-09-13 23:33:38 +0300

    add some data for trying ajax queries

--- data.php
+++ data.php
@@ -1,7 +1,22 @@
 <?php
-$country=["Ru","Ua","Us","Fr"];
-if (isset($_POST['select']) && $_POST['select']=="country"){
-   echo json_encode($country);
+
+$country = ["Ru", "Ua", "Us", "Fr"];
+$city = ["Moskow", "kiev", "washington"];
+$street = ["Pobedi","lenina"];
+$house = ["1","2","123"];
+if (isset($_POST['changed'])) {
+    if ($_POST['changed'] == "country" && $_POST['download'] == "country") {
+        echo json_encode($country);
+    }
+    
+    if ($_POST['changed'] == "country" && $_POST['download'] == "city") {
+        echo json_encode($city);
+    }
+    if ($_POST['changed'] == "city" && $_POST['download'] == "street") {
+        echo json_encode($street);
+    }
+    if ($_POST['changed'] == "street" && $_POST['download'] == "house") {
+        echo json_encode($house);
+    }
 }
-//$city = ["Moskow","kiev","washington"];
 
