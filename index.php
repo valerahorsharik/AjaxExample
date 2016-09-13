2697490d11b8d00738072e274534db1a964c7bac
Valera Horsharik <valera.horsharik@gmail.com>
2016-09-13 23:32:26 +0300

    small changes

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php
-$country=["Ru","Ua","Us","Fr"];
-echo json_encode($country);
+
 ?>
 <html>
     <head>
         <meta charset="utf-8">  
     </head>
     <body>
-        <div class="options">Call centr: 
-            <select id="country">
-            </select>
-        </div>
-        <div class="options">Call centr: 
-            <select id="city">
-            </select>
-        </div>
-        <div class="options">Call centr: 
-            <select>
-                <option value="1">KievExpert</option>
-                <option value="1">ZaporozhyeExpert</option>
-            </select>
-        </div>
-        <div class="options">Call centr: 
-            <select>
-                <option value="1">KievExpert</option>
-                <option value="1">ZaporozhyeExpert</option>
-            </select>
+        <div id="list">
+            <div class="options">Country:
+                <select id="country"> 
+                    <option>Please select...</option>
+                </select>
+            </div>
+            <div class="options">City:
+                <select id="city">
+                    <option>Please select...</option>
+                </select>
+            </div>
+            <div class="options">Street:
+                <select id="street">
+                    <option>Please select...</option>
+                </select>
+            </div>
+            <div class="options">House: 
+                <select id="house">
+                    <option>Please select...</option>
+                </select>
+            </div>
         </div>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script type="text/javascript" src="main.js"></script>
