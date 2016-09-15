d1d6692cb0b741db4ba6603709f91c8a63a88a01
Valera Horsharik <valera.horsharik@gmail.com>
2016-09-15 23:50:48 +0300

    add a DB connection and queries

--- data.php
+++ data.php
@@ -1,21 +1,55 @@
 <?php
-
-$country = ["Ru", "Ua", "Us", "Fr"];
-$city = ["Moskow", "kiev", "washington"];
-$street = ["Pobedi","lenina"];
-$house = ["1","2","123"];
+$host = "localhost";
+$dbname = "ajax_test";
+$user = "root";
+$pass = "";
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+if (!$db) {
+    die("Something happened");
+}
 if (isset($_POST['changed'])) {
-    if ($_POST['changed'] == "country" && $_POST['download'] == "country") {
-        echo json_encode($country);
+    if ($_POST['changed'] == "cc_callcenters" && $_POST['download'] == "cc_callcenters") {
+        $query_result = $db->query('SELECT * FROM cc_callcenters');
+        foreach ($query_result->fetchAll() as $row_number => $row) {
+            $results[] = $row['name'];
+        }
+        
+        $db = NULL;
+        echo json_encode($results);
     }
-    
-    if ($_POST['changed'] == "country" && $_POST['download'] == "city") {
-        echo json_encode($city);
+
+    if ($_POST['changed'] == "cc_callcenters" && $_POST['download'] == "cc_desks") {
+        $query_result = $db->query('SELECT * '
+                . 'FROM cc_desks'
+                . 'LEFT JOIN cc_callcenters'
+                . 'ON cc_desks.id_callcenter = cc_callcenters.id'
+                . 'WHERE cc_callcenters.name = '.$_POST['changed_value']);
+        foreach ($query_result->fetchAll() as $row_number => $row) {
+            $results[] = $row['name'];
+        }
+        
+        $db = NULL;
+        echo json_encode($results);
     }
-    if ($_POST['changed'] == "city" && $_POST['download'] == "street") {
-        echo json_encode($street);
+    if ($_POST['changed'] == "cc_desks" && $_POST['download'] == "cc_teams") {
+       $query_result = $db->query('SELECT * FROM cc_callcenters');
+        foreach ($query_result->fetchAll() as $row_number => $row) {
+            $results[] = $row['name'];
+        }
+   
+        $db = NULL;
+        echo json_encode($results);
     }
-    if ($_POST['changed'] == "street" && $_POST['download'] == "house") {
+    if ($_POST['changed'] == "cc_teams" && $_POST['download'] == "cc_users") {
+        $query_result = $db->query('SELECT * FROM cc_callcenters');
+        foreach ($query_result->fetchAll() as $row_number => $row) {
+            $results[] = $row['name'];
+        }
+   
+        $db = NULL;
+        echo json_encode($results);
+    }
+    if ($_POST['changed'] == "cc_users" && $_POST['download'] == "salestatus") {
         echo json_encode($house);
     }
 }
